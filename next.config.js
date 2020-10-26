815072fceee4bc60e1f90ecd3d8cd579f73617f0
Marvin Hagemeister <hello@marvinh.dev>
2020-10-26 09:22:42 +0100

    Fix nextjs trying to serialize circular props

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const withPrefresh = require('@prefresh/next');
+const preact = require('preact');
 
 const config = {
   experimental: {
@@ -30,13 +31,30 @@ const config = {
     const aliases = config.resolve.alias || (config.resolve.alias = {});
     aliases.react = aliases['react-dom'] = 'preact/compat';
 
-    // inject Preact DevTools
-    if (dev && !isServer) {
-      const entry = config.entry;
-      config.entry = () => entry().then(entries => {
-        entries['main.js'] = ['preact/debug'].concat(entries['main.js'] || []);
-        return entries;
-      });
+    if (dev) {
+      if (isServer) {
+        // Remove circular `__self` and `__source` props only meant for
+        // development. See https://github.com/developit/nextjs-preact-demo/issues/25
+        let oldVNodeHook = preact.options.vnode;
+        preact.options.vnode = (vnode) => {
+          const props = vnode.props;
+          if (props != null) {
+            if ('__self' in props) props.__self = null;
+            if ('__source' in props) props.__source = null;
+          }
+
+          if (oldVNodeHook) {
+            oldVNodeHook(vnode);
+          }
+        }
+      } else {
+        // inject Preact DevTools
+        const entry = config.entry;
+        config.entry = () => entry().then(entries => {
+          entries['main.js'] = ['preact/debug'].concat(entries['main.js'] || []);
+          return entries;
+        });
+      }
     }
 
     return config;
